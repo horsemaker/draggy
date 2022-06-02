5d16391c50f66841eb60dc17c3c2ae66e8603f11
horsemaker <yash_ghodekar@yahoo.in>
2022-06-02 18:40:27 +0530

    add routes & add styles to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Board, BoardCardModal, Title } from "./components";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="mt-8 max-w-4xl m-auto flex flex-col gap-10">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Title text="Draggy🎏" />
+              <Board />
+              <Outlet />
+            </>
+          }
+        >
+          <Route path=":groupId/:taskId" element={<BoardCardModal />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
